c6565dc8a43b14a672b2b398ecec7e1ca24b5e11
Julian Ward <Juliansw@usca.edu>
2025-02-05 14:07:19 -0500

    Added support for .env file

--- services/api.js
+++ services/api.js
@@ -1,8 +1,8 @@
-API_URL='https://y5pyf47rw4.execute-api.us-east-2.amazonaws.com/dev/'
+import {API_URL, API_KEY} from '@env'
 
 export const getHelloWorld = async () => {
 
-    const response = await fetch(`${API_URL}helloWorld`)
+    const response = await fetch(`${API_URL}helloWorld`);
     const data = await response.json();
     console.log(API_URL)
     return data.body;
