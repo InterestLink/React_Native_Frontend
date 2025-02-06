c8d5ec92601f843ca2bc80e223a5f4d376cc71fa
gewlance <gewlance@gmail.com>
2025-02-06 01:09:34 -0500

    Added search bar swag 2.6.25

--- components/screens/Search.js
+++ components/screens/Search.js
@@ -1,11 +1,38 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, SafeAreaView, TextInput, StyleSheet } from 'react-native';
 import StyleExample from '../styles/StyleExample';
 
-export default function Search({navigation}) {
+export default function Search({ navigation }) {
+    const [searchQuery, setSearchQuery] = useState(''); // State to store input data
     return (
-        <View style={StyleExample.container}>
-            <Text>In Search</Text>
-        </View>
-    )
+        <SafeAreaView style={StyleExample.container}>
+            <Text style={styles.header}>Link Your Interest! It's Swaggy!!!</Text>
+            <TextInput 
+                style={styles.input} 
+                placeholder="Search..." 
+                value={searchQuery} 
+                onChangeText={setSearchQuery} // Updates state with input
+            />
+            <Text style={styles.resultText}>You searched for: {searchQuery}</Text>
+
+        </SafeAreaView>
+    );
 }
+
+const styles = StyleSheet.create({
+    header: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 30,
+    },
+    input: {
+        width: '90%',
+        height: 40,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        marginBottom: 550,
+    },
+});
