b3aa0bd50473726419d1452eb9c925f5a06b240f
Joshua Rogers <jmr46@usca.edu>
2025-04-13 19:03:15 -0400

    removed a "/" from getHomepage

--- services/api.js
+++ services/api.js
@@ -121,7 +121,7 @@ export const getPosts = async (parameters) => {
 
 // parameters = { id: 123, isUser: true, userSaved: true, userLiked: false  } Returns communities or profile posts dependent on isUser, if isUser, check if userSaved or userLinked to return liked/saved posts or false on both for default. (id, username, content, image)
 export const getHomepage = async (parameters) => {
-  const url = `${API_URL}/getHomepage`;
+  const url = `${API_URL}getHomepage`;
 
   console.log("📡 Fetching posts from:", url);
 
